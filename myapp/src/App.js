509d0fd78861806dabd8d013a67c4d8dd38b9e0d
Anustha Pal <anusthapal2000@gmail.com>
2025-09-09 03:01:35 +0000

    my react app

---
+++ myapp/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>This is my React test application</h1>
+    </div>
+  );
+}
+
+export default App;
+
