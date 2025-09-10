19d2979e32cc795ef8aa75520265a2299b39e7b5
Anustha Pal <85693752+anustha-pal@users.noreply.github.com>
2025-09-10 07:38:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>This is my React test application-realase1</h1>
+      <h1>This is my React test application-realase2</h1>
     </div>
   );
 }
